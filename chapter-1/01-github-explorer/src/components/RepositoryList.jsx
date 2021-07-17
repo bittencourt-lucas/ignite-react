016b81bbd50593cef02de3b7dc4e7b9ca746fda2
Lucas Bittencourt <lucas@pbhub.com.br>
2021-07-17 18:37:52 -0300

    First React component

---
+++ chapter-1/01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,40 @@
+const repositoryName = "the8bitts";
+
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositórios</h1>
+      
+      <ul>
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>Forms in React</p>
+          <a href="/">
+            Acessar Repositório
+          </a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="/">
+            Acessar Repositório
+          </a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="/">
+            Acessar Repositório
+          </a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="/">
+            Acessar Repositório
+          </a>
+        </li>
+      </ul>
+    </section>
+  )
+}
